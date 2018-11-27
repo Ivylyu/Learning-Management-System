abd1b2111e64bc1f18de3eb82d02e0461dfb59a5
Ivy <erer1995@outlook.com>
2018-11-27 11:56:33 +1100

    add index.css

--- LMS/src/index.js
+++ LMS/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'index.css';
 import Router from "./pages/Router";
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
