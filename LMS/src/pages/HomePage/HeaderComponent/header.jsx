efe52bf9c72973f59e3139f899cc4e195849bcd8
Ivy <erer1995@outlook.com>
2018-11-27 20:41:26 +1100

    add travis ci

---
+++ LMS/src/pages/HomePage/HeaderComponent/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+    <div className="headerContainer">
+        <ul className="header">
+            <li>Sign Up</li>
+            <li>Dashboard</li>
+            <li>Profile</li>
+        </ul>
+    </div>
+);
+export default Header;
--- LMS/src/pages/HomePage/Home.jsx
+++ LMS/src/pages/HomePage/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from './FooterComponent/Footer';
 import LoginForm from './LoginFormComponent/components/LoginForm';
+import Header from './HeaderComponent/Header';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 const Home = () => (
     <div>
+        <Header />
         <LoginForm />
         <Footer /> 
     </div>
