ebd711e7e4c23578085e1a659abe5044171be714
Ivy <erer1995@outlook.com>
2018-12-03 14:53:30 +1100

    add router

--- LMS/src/pages/HomePage/FooterComponent/Footer.jsx
+++ LMS/src/pages/HomePage/FooterComponent/Footer.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import HelpAndSupport from './components/HelpAndSupport';
 import QuickLink from './components/QuickLink';
-import 'typeface-roboto';
 
 class Footer extends React.Component {
     render() {
--- LMS/src/pages/HomePage/HeaderComponent/header.jsx
+++ LMS/src/pages/HomePage/HeaderComponent/header.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './styles.css';
-import 'typeface-roboto';
 
 const Header = () => (
     <div className="headerContainer">
